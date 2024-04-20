92fc19a402fa9f21f28dd91e7cf22e0eaffd03ae
smita-involead <smitarani.sahu@involead.com>
2024-04-20 20:14:14 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/pages/Layout";
+import Home from './components/pages/Home';
+import Contact from "./components/pages/Contact";
+import LoginReg from "./components/pages/auth/LoginReg";
+import SendPasswordResetEmail from "./components/pages/auth/SendPasswordResetEmail";
+import ResetPassword from "./components/pages/auth/ResetPassword";
+import Dashboard from "./components/pages/Dashboard";
+import { useSelector } from "react-redux";
+import ChatBot from "./components/Chatbot";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ // Correctly access the access_token without destructuring
+ const accessToken = useSelector(state => state.auth ? state.auth.access_token : null);
+
+ return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="loginreg" element={<LoginReg />} />
+            <Route path="sendpasswordresetemail" element={<SendPasswordResetEmail />} />
+            <Route path="api/user/reset/:id/:token" element={<ResetPassword />} />
+          </Route>
+          <Route path="/dashboard" element={accessToken ? <Dashboard /> : <Navigate to="/loginreg" />} />
+          <Route path="/chatbot" element={accessToken ? <ChatBot /> : <Navigate to="/loginreg" />} />
+          <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+ );
 }
 
 export default App;
